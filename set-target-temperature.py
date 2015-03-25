b9ef648224a856fa3d4d34f9f979bc378c0f27f3
Nao Taco <naotaco@gmail.com>
2015-03-26 00:04:25 +0900

    Fix error handling

--- set-target-temperature.py
+++ set-target-temperature.py
@@ -5,7 +5,7 @@ import sys
 argvs = sys.argv
 if (len(argvs) < 2):
     print ("specify target temerature for first argument.")
-    exit
+    sys.exit(1)
 
 r = redis.Redis(host='127.0.0.1', port=6379, db=0)
 
@@ -14,12 +14,11 @@ try:
     tmp = float(argvs[1])
 except ValueError:
     print ("Invalid argument. specify float value.")
-    exit
+    sys.exit(1)
 
 if (r.set('cooker_target_temperature', argvs[1])):
     print ("succeed.")
 else:
     print ("failed to set value")
 
-
-print (b"setpoint: " + r.get('cooker_target_temperature'))
+print (b"current setpoint: " + r.get('cooker_target_temperature'))
