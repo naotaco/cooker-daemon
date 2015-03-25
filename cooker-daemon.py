c2a60a788536651165bf765537e8756593666f6c
Nao Taco <naotaco@gmail.com>
2015-03-25 22:29:45 +0900

    Improve behavior before shutdown

--- cooker-daemon.py
+++ cooker-daemon.py
@@ -11,11 +11,15 @@ from max31855 import MAX31855, MAX31855Error
 import atexit
 import RPi.GPIO as GPIO
 
-atexit.register(GPIO.cleanup)
-
-cooker_pin = 35 # pin number connected to relay for the cooker
 GPIO.setmode(GPIO.BOARD) # it seems default mode doesn't work on RPi2
 GPIO.setup(cooker_pin, GPIO.OUT)
+
+# setup pin numbers
+cooker_pin = 35 # pin number connected to relay for the cooker
+cs_pin = 24 # MAX31855's chip select
+clock_pin = 23
+data_pin = 21
+units = "c" # Fahrenheight? what's that?
 
 def turn_on():
     GPIO.output(cooker_pin, True)
@@ -23,18 +27,30 @@ def turn_on():
 def turn_off():
     GPIO.output(cooker_pin, False)
 
+def _on_exit():
+    print ("at exit")
+    turn_off()
+    time.sleep(1)
+    GPIO.cleanup()
+    print ("exit handler triggered")
+    time.sleep(1)
 
-cs_pin = 24 # MAX31855's chip select
-clock_pin = 23
-data_pin = 21
-units = "c" # Fahrenheight? what's that?
+def on_exit(sig, func=None):
+    _on_exit()
+
+# register function to run before shutdown
+import signal
+signal.signal(signal.SIGTERM, on_exit)
+atexit.register(_on_exit)
+
 
 log_file_name = "/tmp/cooker_log_" + datetime.now().isoformat() + ".txt"
 logging.basicConfig(level=logging.DEBUG, filenamae=log_file_name)
 
 thermocouple = MAX31855(cs_pin, clock_pin, data_pin, units, GPIO.BOARD)
 
-setpoint = 64.0 # default, and best temperature for chicken breast
+# setpoint = 64.0 # default, and best temperature for chicken breast
+setpoint = 80
 
 running = True
 while(running):
@@ -63,8 +79,10 @@ while(running):
 
     except KeyboardInterrupt:
         running = False
+        turn_off()
+        time.sleep(1)
+        
 
     time.sleep(1)
-
 
 thermocouple.cleanup()
